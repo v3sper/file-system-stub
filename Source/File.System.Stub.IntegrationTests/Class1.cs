83a9c3d0b5d1daea16032a0a87da45b059f91a2a
v3sper <patryk90.stepien@gmail.com>
2018-10-04 20:31:51 +0200

    Clean solution

--- Source/File.System.Stub.IntegrationTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace File.System.Stub.IntegrationTests
-{
-    public class Class1
-    {
-    }
-}
--- Source/File.System.Stub.UnitTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace File.System.Stub.UnitTests
-{
-    public class Class1
-    {
-    }
-}
