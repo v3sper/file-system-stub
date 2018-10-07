cc5eb2a31bc7b17d1264c5b730c830473e70f177
v3sper <patryk90.stepien@gmail.com>
2018-10-07 13:52:52 +0200

    Add suffix creation based on branch

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,19 @@
-.paket/paket pack nuget-files --version 0.0.0-alpha
+NUGET_TOKEN=$1
+NUGET_SOURCE=$2
+TRAVIS_BRANCH=$3
+TRAVIS_BUILD_NUMBER=$4
+
+if [[ $TRAVIS_BRANCH =~ "^fss-*" ]]
+then
+    SUFFIX = "-alpha"
+elif [[ $TRAVIS_BRANCH = "develop" ]]
+then
+    SUFFIX = "-beta"
+elif [[ $TRAVIS_BRANCH =~ "^(master$|release-*)" ]]
+then
+    SUFFIX = ""
+else
+	return
+fi
+
+.paket/paket pack nuget-files --version "0.0.0-$SUFFIX"
