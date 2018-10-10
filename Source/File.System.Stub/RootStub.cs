073230707f983acb781a821f1908acefedb03ee2
Patryk Spytkowski <patryk.spytkowski@relativity.com>
2018-10-10 14:51:22 +0200

    Another Equals and GetHashCodeRemoval

--- Source/File.System.Stub/RootStub.cs
+++ Source/File.System.Stub/RootStub.cs
@@ -6,19 +6,5 @@ namespace File.System.Stub
 	{
 		public string Path { get; set; }
 		public DirectoryStub Directory { get; set; }
-
-		public override bool Equals(object obj)
-		{
-			var stub = obj as RootStub;
-			return stub != null && Path == stub.Path;
-		}
-
-		public override int GetHashCode()
-		{
-			var hashCode = -1342469650;
-			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Path);
-			hashCode = hashCode * -1521134295 + EqualityComparer<DirectoryStub>.Default.GetHashCode(Directory);
-			return hashCode;
-		}
 	}
 }
